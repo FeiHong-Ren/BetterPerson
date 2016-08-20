e5bf25b95437d3dba8f061de55279e78c2e41de0
FeiHong Ren <FeiHong Ren>
2016-08-20 15:38:44 -0400

    add the add activity title and time

--- app/src/main/java/com/feihongren/betterperson/AddActiviy.java
+++ app/src/main/java/com/feihongren/betterperson/AddActiviy.java
@@ -3,8 +3,11 @@ package com.feihongren.betterperson;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
 
 public class AddActiviy extends AppCompatActivity {
 
@@ -15,6 +18,14 @@ public class AddActiviy extends AppCompatActivity {
         //set up the action tool bar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.add_toolbar);
         setSupportActionBar(myToolbar);
+
+        //set the limit for add activity hour range
+        EditText hourLimit = (EditText) findViewById(R.id.add_time_hour);
+        hourLimit.setFilters(new InputFilter[]{ new InputFilterMinMax(0, 23)});
+
+        //set the limit for add activity minute range
+        EditText minuteLimit = (EditText) findViewById(R.id.add_time_minute);
+        minuteLimit.setFilters(new InputFilter[]{ new InputFilterMinMax(0, 59)});
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

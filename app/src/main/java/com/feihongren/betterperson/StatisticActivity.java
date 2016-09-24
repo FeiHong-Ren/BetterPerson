d65a0ab19663be5028fda5c2f9467fb429cf179f
FeiHong Ren <FeiHong Ren>
2016-09-24 14:50:22 -0400

    complete the function of statistic activity

--- app/src/main/java/com/feihongren/betterperson/StatisticActivity.java
+++ app/src/main/java/com/feihongren/betterperson/StatisticActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -32,6 +34,10 @@ public class StatisticActivity extends AppCompatActivity {
     private ArrayAdapter<String> monthAdpater;
     private ArrayList<String> yearList;
     private ArrayList<String> monthList;
+    private GraphView graphView;
+    private ArrayList<Integer> monthPointArray;
+    private int selectedYear;
+    private int selectedMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +50,17 @@ public class StatisticActivity extends AppCompatActivity {
         dbHandler = new DBHandler(this);
 
         Calendar c = Calendar.getInstance();
-        int month = c.get(Calendar.MONTH)+ 1;
-        int year = c.get(Calendar.YEAR);
+        selectedMonth = c.get(Calendar.MONTH)+ 1;
+        selectedYear = c.get(Calendar.YEAR);
         //Calculate the how many days in the month
-        Calendar mycal = new GregorianCalendar(year, month-1, 1);
+        Calendar mycal = new GregorianCalendar(selectedYear, selectedMonth-1, 1);
         int daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
 
         yearSpinner = (Spinner) findViewById(R.id.statistic_year_spinner);
         monthSpinner = (Spinner) findViewById(R.id.statistic_month_spinner);
         yearList = new ArrayList<>();
         monthList = new ArrayList<>();
-        for (int i = 2012; i <= year; i++) {
+        for (int i = 2016; i <= selectedYear; i++) {
             yearList.add(Integer.toString(i));
         }
 
@@ -75,12 +81,59 @@ public class StatisticActivity extends AppCompatActivity {
         monthAdpater = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,monthList);
         yearSpinner.setAdapter(yearAdpater);
         monthSpinner.setAdapter(monthAdpater);
-        yearSpinner.setSelection(year-2012);
-        monthSpinner.setSelection(month-1);
+        yearSpinner.setSelection(selectedYear-2016);
+        monthSpinner.setSelection(selectedMonth-1);
+
+        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedMonth = i+1;
+                monthPointArray.clear();
+                monthPointArray=dbHandler.getSelectedMonthPoint(i+1,selectedYear);
+                graphView.removeAllSeries();
+                LineGraphSeries<DataPoint> newSeries= new LineGraphSeries<>();
+                Calendar myCalendar = new GregorianCalendar(selectedYear, selectedMonth-1, 1);
+                int daysInMonth = myCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                for(int j=1;j<=daysInMonth;j++){
+                    newSeries.appendData(new DataPoint(j,monthPointArray.get(j-1)),true,31);
+                }
+                graphView.addSeries(newSeries);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedYear = 2016+i;
+                monthPointArray.clear();
+                monthPointArray=dbHandler.getSelectedMonthPoint(selectedMonth,2016+i);
+                graphView.removeAllSeries();
+                LineGraphSeries<DataPoint> newSeries= new LineGraphSeries<>();
+                Calendar myCalendar = new GregorianCalendar(selectedYear, selectedMonth-1, 1);
+                int daysInMonth = myCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                for(int j=1;j<=daysInMonth;j++){
+                    newSeries.appendData(new DataPoint(j,monthPointArray.get(j-1)),true,31);
+                }
+                graphView.addSeries(newSeries);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
 
         //Init the data for current month
-        GraphView graphView = (GraphView) findViewById(R.id.statistic_graph_view);
-        ArrayList<Integer> monthPointArray= dbHandler.getSelectedMonthPoint(month,year);
+        graphView = (GraphView) findViewById(R.id.statistic_graph_view);
+        monthPointArray = dbHandler.getSelectedMonthPoint(selectedMonth,selectedYear);
         series = new LineGraphSeries<>();
         for(int i=1;i<=daysInMonth;i++){
             series.appendData(new DataPoint(i,monthPointArray.get(i-1)),true,31);
